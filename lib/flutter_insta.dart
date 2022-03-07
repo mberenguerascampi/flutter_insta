d016650e8093be97d2420dc105707a245089c957
Marc Berengueras <mberengueras@nexusgeographics.com>
2022-03-07 14:35:25 +0100

    Fix profile request

--- lib/flutter_insta.dart
+++ lib/flutter_insta.dart
@@ -21,7 +21,13 @@ class FlutterInsta {
 
   //get profile details
   Future<void> getProfileData(String username) async {
-    var res = await http.get(Uri.parse(Uri.encodeFull(url + username + "/?__a=1")));
+     Map<String, String> _headers = {
+      "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0",
+      "Cookie":
+          'csrftoken=KwugFcdUbqWmIw5gafx4XceFqG7lL0iy; mid=YXGdmQAEAAGZ8KAkPxbouItPlKZW;ig_did=B1A4CD44-2BC0-46D6-9EA8-183539CC48D0;rur="ASH\0541626909116\0541666509625:01f790737601ddb28113116c4f532ec31a70ba9eed81c465a824b04048f33062c5ad07d8"; ds_user_id=1626909116; sessionid=1626909116%3ASJKuakuknKMwgK%3A29; shbid="13780\0541626909116\0541666509141:01f7550222f10cb6d3fe921d0365f3c16d488e2b5fa5d8df394a2e22bf77b6e58c651ba9"; shbts="1634973141\0541626909116\0541666509141:01f76d9c1887499197411336f50b5e86b6bd269a7d945cef01282c80c8f395ac42412c0f"'
+    };
+    var res = await http.get(Uri.parse(Uri.encodeFull(url + username + "/channel/?__a=1")), headers: _headers);
     var data = json.decode(res.body);
     var graphql = data['graphql'];
     var user = graphql['user'];
